0a541e9ae680b35283c88050f795f41776f13508
Mitchell ONeillgit config --global user.name Mitchell <7moneill@gmail.com>
2016-10-14 21:13:25 +0000

    added confirmation emails to orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,9 +8,12 @@ class OrdersController < ApplicationController
     charge = perform_stripe_charge
     order  = create_order(charge)
 
+
+
     if order.valid?
       empty_cart!
       redirect_to order, notice: 'Your Order has been placed.'
+      Notifier.order_confirmation(order).deliver_now
     else
       redirect_to cart_path, error: order.errors.full_messages.first
     end
---
+++ app/mailers/notifier.rb
@@ -0,0 +1,13 @@
+class Notifier < ApplicationMailer
+  default from: 'no-reply@jungle.com',
+          return_path: 'system@example.com'
+
+
+  def order_confirmation(order)
+    @order = order
+    mail(to: @order.email,
+      subject: "Your Jungle order: \##{@order.id}has been processed"
+      )
+  end
+
+end
